2ad748f131a4012818589101dea4eb7f89effae0
Tim Cassels <cassels2025@gmail.com>
2016-08-02 16:52:37 -0400

    Fixed terminal scrolling and added a clear line function

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,12 +8,6 @@ void kmain(void) {
 	int i = 0; // Kernel loading counter
 
 	clear_screen();
-	printk("Loading Kernel.....Standby\n");
-	printk("\n");
-	for (i=0; i<10; i++) {
-		//sleep(1);
-		printk("..\n");
-	}
-	printk("Kernel Loaded, begin driver init\n");
-	printk("Driver init failed, no drivers dummy\n");
+	printk("Kernel Loaded\n");
+	printk("hlt\n");
 }
--- src/kernel/printk.c
+++ src/kernel/printk.c
@@ -1,30 +1,41 @@
 char *vidptr = (char*)0xb8000;
 int line = 0; // Used to control which line we're on
-int i = 0;    // Line char position persistence
+int linechar = 0;
 
-void terminal_scroll() {
-    for (int i = 0; i < 25; i++) {
-        for (int m = 0; m < 80; ++m) {
-            vidptr[i*80+m] = vidptr[(i+1)*80+m];
-        }
+void clear_line(int l) {
+    for (int c = 0; c < 80; c++) {
+        vidptr[l*80*2+c] = " ";
+        c++;
     }
 }
 
-void printk(char *str) {
-    if (line == 25) {
-        terminal_scroll();
+void terminal_scroll() {
+    for (int i = 0; i < 25; i++) {
+        for (int m = 0; m < 80; m++) {
+            vidptr[i*80*2+m] = vidptr[(i+1)*80*2+m];
+        }
     }
+    clear_line(25);
+}
+
+void printk(char *str) {
     int j = 0; // Specifies which item in the *str char array
     int len = strlen(str);
-    for (int j = 0; j < len; j++) {
+    for (j = 0; j < len; j++) {
         switch (str[j]) {
-            case '\n':
-                line++;
-                i = (line*80*2);
+            case '\n':  // New line char = new line :)
+                if (line <= 23) {
+                    line++;
+                } else {
+                    terminal_scroll();
+                }
+                linechar = (line*80*2);
+            case '\0':  // Work around so we don't print the very last item
+                break;
             default:
-                vidptr[i] = str[j];
-                vidptr[i+1] = 0x07;
-                i = i + 2;
+                vidptr[linechar] = str[j];
+                vidptr[linechar+1] = 0x07;
+                linechar = linechar + 2;
         }
     }
 }
